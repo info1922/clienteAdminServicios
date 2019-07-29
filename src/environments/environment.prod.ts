933b4dc99bb170d0140934a4367ce182cac96297
info1922 <ivann1922@gmail.com>
2019-07-29 15:10:32 -0500

    Modificacion de variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'https://serviciosmiahutlan.herokuapp.com/api',
+  wsURL: 'https://serviciosmiahutlan.herokuapp.com'
 };
